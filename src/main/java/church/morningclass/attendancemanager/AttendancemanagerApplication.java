64c424d6d35dacae1c29ac1bd13fee73e46fb016
David <podavid@connect.hku.hk>
2021-08-21 01:43:40 +0800

    Initial commit

---
+++ src/main/java/church/morningclass/attendancemanager/AttendancemanagerApplication.java
@@ -0,0 +1,13 @@
+package church.morningclass.attendancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendancemanagerApplication.class, args);
+    }
+
+}
